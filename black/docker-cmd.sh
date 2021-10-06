d4eadbf23a6dbf482716e03e5f2ce3f05140a17e
chrysa <chrysa@users.noreply.github.com>
2021-10-06 14:18:30 +0000

    update packages and rework cmd entrypoints

--- black/docker-cmd.sh
+++ black/docker-cmd.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
 cmd="black"
+<<<<<<< HEAD
+if [ -f "/app/black.toml" ]; then
+    cmd="${cmd} --config=/app/black.toml"
+elif [ -f "/app/pyproject.toml" ]; then
+    cmd="${cmd} --config=/app/pyproject.toml"
+fi
+
+${cmd} /app/**/*.py
+=======
 if [ -f "./black.toml" ]; then
     cmd="${cmd} --config=./black.toml"
 fi
 cmd="${cmd} /app/**/*.py"
 
 ${cmd}
+>>>>>>> a08bd37ec7c9e9fc8edc2ed06cea74144553b2c2
--- pylint/docker-cmd.sh
+++ pylint/docker-cmd.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
 cmd="pylint"
+<<<<<<< HEAD
+if [ -f "/app/setup.cfg" ]; then
+    cmd="${cmd} --rcfile=/app/setup.cfg"
+elif [ -f "/app/.pylintrc" ]; then
+    cmd="${cmd} --rcfile=/app/.pylintrc"
+fi
+${cmd} /app/**/*.py
+=======
 if [ -f "./setup.cfg" ]; then
     cmd="${cmd} --rcfile=./setup.cfg"
 fi
 cmd="${cmd} /app/**/*.py"
 
 ${cmd}
+>>>>>>> a08bd37ec7c9e9fc8edc2ed06cea74144553b2c2
