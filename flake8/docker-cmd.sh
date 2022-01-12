9fe94efd8af8c31139085a76e10c0a2ace86b466
chrysa <chrysa@users.noreply.github.com>
2022-01-12 16:16:16 +0100

    test update

--- flake8/docker-cmd.sh
+++ flake8/docker-cmd.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/sh
 
 cmd="flake8"
@@ -9,3 +10,37 @@ elif [ -f "/app/.flake8" ]; then
     cmd="${cmd} --config=/app/.flake8"
 fi
 ${cmd} /app/**/*.py
+=======
+#!/bin/sh
+
+cmd="flake8"
+
+files=/app/**/*.py
+
+if [ -f "/app/setup.cfg" ]; then
+    config="--config=/app/setup.cfg"
+elif [ -f "/app/tox.ini" ]; then
+    config="--config=/app/tox.ini"
+elif [ -f "/app/.flake8" ]; then
+    config="--config=/app/.flake8"
+fi
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --config)
+            config="--config=$1"
+            shift 2
+        ;;
+        --file)
+            files="$1"
+            shift 2
+        ;;
+        --git)
+            files=`git status | grep -E "modified" | grep ".py$" | cut -d":" -f2 | xargs`
+            shift 2
+        ;;
+    esac
+done
+
+${cmd} ${config} $files
+>>>>>>> d98b6dc (test update)
--- mypy/docker-cmd.sh
+++ mypy/docker-cmd.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/bin/sh
 
 cmd="mypy"
@@ -11,3 +12,39 @@ elif [ -f "/app/pyproject.toml" ]; then
     cmd="${cmd} --config=/app/pyproject.toml"
 fi
 ${cmd} /app/**/*.py
+=======
+#!/bin/sh
+
+cmd="mypy"
+
+files=/app/**/*.py
+
+if [ -f "/app/setup.cfg" ]; then
+    config="--config=/app/setup.cfg"
+elif [ -f "/app/mypy.ini" ]; then
+    config="--config=/app/mypy.ini"
+elif [ -f "/app/.mypy.ini" ]; then
+    config="--config=/app/.mypy.ini"
+elif [ -f "/app/pyproject.toml" ]; then
+    config="--config=/app/pyproject.toml"
+fi
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --config)
+            config="--config=$1"
+            shift 2
+        ;;
+        --file)
+            files="$1"
+            shift 2
+        ;;
+        --git)
+            files=`git status | grep -E "modified" | grep ".py$" | cut -d":" -f2 | xargs`
+            shift 2
+        ;;
+    esac
+done
+
+${cmd} ${config} $files
+>>>>>>> d98b6dc (test update)
