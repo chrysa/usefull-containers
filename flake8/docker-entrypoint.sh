b6447f07668c2770d0b23d4f96ab32479efa803f
chrysa <greau.anthony@gmail.com>
2023-03-29 23:20:35 +0200

    fix flake8

---
+++ flake8/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cmd="flake8"
+
+if [ -f "/app/setup.cfg" ]; then
+    config="--config=/app/setup.cfg"
+elif [ -f "/app/tox.ini" ]; then
+    config="--config=/app/tox.ini"
+elif [ -f "/app/.flake8" ]; then
+    config="--config=/app/.flake8"
+else
+    config="--config=/opt/setup-default.cfg"
+fi
+
+set -x
+${cmd} ${config} $(find /app -name "*.py")
