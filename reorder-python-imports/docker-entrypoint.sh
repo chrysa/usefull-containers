102fe066c07c7c223346fe88bb6ec66c643e609a
chrysa <greau.anthony@gmail.com>
2023-03-30 17:05:43 +0200

    reorder-python-imports update

---
+++ reorder-python-imports/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+echo "====>> run reorder python imports"
+cmd="reorder-python-imports"
+
+config="--py3-plus --application-directories=."
+
+
+if [ -z "$@" ]; then
+    files=$(find /app -name "*.py")
+else 
+    files="$@"
+fi
+
+set -x
+${cmd} ${config} ${files}
