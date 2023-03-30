d8671b1823cbd5e9e404262aef61254ec9af0bdf
chrysa <chrysa@users.noreply.github.com>
2023-03-30 09:58:43 +0200

    mypy update

---
+++ mypy/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+cmd="mypy"
+
+if [ -f "/app/setup.cfg" ]; then
+    config="--config=/app/setup.cfg"
+elif [ -f "/app/mypy.ini" ]; then
+    config="--config=/app/mypy.ini"
+elif [ -f "/app/.mypy.ini" ]; then
+    config="--config=/app/.mypy.ini"
+elif [ -f "/app/pyproject.toml" ]; then
+    config="--config=/app/pyproject.toml"
+else
+    config="--config=/opt/setup-default.cfg"
+fi
+
+if [ -z "$@" ]; then
+    files=$(find /app -name "*.py")
+else 
+    files="$@"
+fi
+
+set -x
+${cmd} ${config} ${files}
